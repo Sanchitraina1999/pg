a67e9db0dd5c9219c1d10c882c88cb7408aeb055
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-05-12 17:07:26 +0530

    --- Auto-Git Commit ---

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import string
 import random
 
+def randomString(stringLength=8):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
 if __name__ == '__main__':
     s = string.ascii_lowercase
     s += string.ascii_uppercase
