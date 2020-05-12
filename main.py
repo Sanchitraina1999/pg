fcee02b73ca95bdfecfb84a5ffbb45fcee69484d
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-05-12 17:11:32 +0530

    --- Auto-Git Commit ---

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import string
 import random
 
 def randomString(letters,stringLength=8):
-    return ''.
     return ''.join(random.choice(letters) for i in range(stringLength))
 
 if __name__ == '__main__':
