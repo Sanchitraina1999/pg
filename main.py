8ca4a939a1a68c5deed79516cec7a06a3dff6a34
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-05-12 17:06:31 +0530

    --- Auto-Git Commit ---

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import string
 import random
 
 if __name__ == '__main__':
-    s1 = string.ascii_lowercase
-    s2 = string.ascii_uppercase
+    s = string.ascii_lowercase
+    s += string.ascii_uppercase
     s3 = string.digits
     s4 = string.punctuation
-    
     s = s1 + s2 + s3 + s4
-
     print(s)    
+    length = int(input("Enter required length of password: "))
     
-    length = int(input("Enter re"))
+    
