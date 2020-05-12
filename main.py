a86c785c02ad6a498f7c1bb39fc82d33340ee964
Sanchit Raina <sanchitraina1999@gmail.com>
2020-05-12 17:25:50 +0530

    :fire:

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ import random
 def randomString(letters,stringLength=8):
     return ''.join(random.choice(letters) for i in range(stringLength))
 
-if __name__ == '__main__':
-    s = string.ascii_lowercase
-    s += string.ascii_uppercase
-    s += string.digits
-    s += string.punctuation  
-    length = int(input("Enter required length of password: "))
-    print("Password: ", randomString(s,length))
+s = string.ascii_lowercase
+s += string.ascii_uppercase
+s += string.digits
+s += string.punctuation  
+length = int(input("Enter required length of password: "))
+print("Password: ", randomString(s,length))
